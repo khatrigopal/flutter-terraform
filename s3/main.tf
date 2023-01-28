1da6a1bea12e83a03494137e0caa0c75d703f48c
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:26:26 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -4,4 +4,24 @@ module "dev-s3" {
   tags       = var.tags
   versioning = var.versioning
   acl        = var.s3_acl
+  policy = data.aws_iam_policy_document.public_read_access.json
+}
+#DataSource to generate a policy document
+data "aws_iam_policy_document" "public_read_access" {
+  statement {
+    principals {
+	  type = "*"
+	  identifiers = ["*"]
+	}
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      var.s3_buckets,
+      "${var.s3_buckets}/*",
+    ]
+  }
 }
