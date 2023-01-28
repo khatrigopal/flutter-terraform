e4dccdba391fd9a0fab2efddbf05afe4b4d36124
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 22:30:52 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,5 @@
 module "s3" {
   source     = "../modules/s3"
-  bucket_name = "${var.s3_buckets}"
-  tags       = "${var.tags}"
-  versioning = var.versioning
-  acl        = var.s3_acl
+  bucket_name = "${var.bucket_name}"
+  acl        = var.acl_value
 }
