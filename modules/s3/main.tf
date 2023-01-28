a8b872b86638d9f279b27e451dc34fe0034c04cc
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:45:34 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,3 +14,11 @@ resource "aws_s3_bucket_acl" "acl" {
   bucket = aws_s3_bucket.s3-bucket.id
   acl    = var.acl
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.s3-bucket.id
+  block_public_acls       = var.s3_block_public_acls
+  block_public_policy     = var.s3_block_public_policy
+  ignore_public_acls      = var.s3_ignore_public_acls
+  restrict_public_buckets = var.s3_restrict_public_buckets
+}
