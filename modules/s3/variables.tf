f7bc46a4dcb0a8806ceee9232bdc8e3f2f8b825d
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:01:37 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,3 +1,27 @@
-variable "bucket_name" {}
+variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "us-east-1"
+}
 
-variable "acl_value" {}
+variable "s3_buckets" {
+  type        = string
+  description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "(Optional) A mapping of tags to assign to the bucket."
+  default     = {}
+}
+
+variable "acl" {
+  type        = string
+  description = " Defaults to private "
+  default     = "private"
+}
+
+variable "versioning" {
+  description = "versioning config"
+  type        = string
+  default     = "Enabled"
+}
