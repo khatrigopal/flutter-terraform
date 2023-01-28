07d7c83be9577ed27365ab24f9c5e7e91c212c13
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 22:28:40 +0530

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,14 +1,5 @@
-variable "aws_region" {
-  description = "The AWS region to use to create resources."
-  default     = "us-east-1"
+variable "bucket_name" {}
+
+variable "acl_value" {
+    default = "private"
 }
-
-
-
-variable "bucket_name" {
-  type        = string
-  description = "Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-  default     = ""
-}
-
-
