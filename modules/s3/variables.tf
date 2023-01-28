3fd40acd26528c44afbd958b352bed4a19d96c87
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:46:20 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -20,13 +20,45 @@ variable "acl" {
   default     = "private"
 }
 
-variable "versioning" {
-  description = "versioning config"
-  type        = string
-  default     = "Enabled"
-}
 variable "bucket_force_destroy" {
   description = ""
   type        = bool
   default     = "false"
+}
+variable "attach_policy" {
+  description = ""
+  type        = bool
+  default     = "false"
+}
+
+variable "s3_block_public_acls" {
+  description = "s3_block_public_acls"
+  type        = bool
+  default     = true
+}
+variable "s3_block_public_policy" {
+  description = "s3_block_public_policy"
+  type        = bool
+  default     = true
+}
+variable "s3_ignore_public_acls" {
+  description = "s3_ignore_public_acls"
+  type        = bool
+  default     = true
+}
+variable "s3_restrict_public_buckets" {
+  description = "s3_restrict_public_buckets"
+  type        = bool
+  default     = true
+}
+
+variable "s3_acl" {
+  description = "acl access"
+  type        = string
+  default     = "private"
+}
+variable "versioning" {
+  description = "versioning config"
+  type        = string
+  default     = "Enabled"
 }
