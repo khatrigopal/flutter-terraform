77d8baa62017ce0112d73e871e26486462729f87
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:03:06 +0530

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,6 +1,39 @@
-variable "bucket_name" {}
-
-variable "acl_value" {
-    default = "private"
+variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "us-east-1"
 }
 
+variable "s3_buckets" {
+  type        = string
+  description = "Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+  default     = ""
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "(Optional) A mapping of tags to assign to the bucket."
+  default     = {}
+}
+
+variable "acl" {
+  type        = string
+  description = " Defaults to private "
+  default     = "private"
+}
+
+variable "bucket_force_destroy" {
+  description = ""
+  type        = bool
+  default     = "false"
+}
+
+variable "s3_acl" {
+  description = "acl access"
+  type        = string
+  default     = "private"
+}
+variable "versioning" {
+  description = "versioning config"
+  type        = string
+  default     = "Enabled"
+}
