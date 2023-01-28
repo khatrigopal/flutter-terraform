4ccf82c4cb13405a06e6001da6c4e4d4c12e20ea
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 23:00:44 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,16 @@
-resource "aws_s3_bucket" "demos3" {
-    bucket = "${var.bucket_name}" 
-    acl = "${var.acl_value}"   
-    
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket        = var.s3_buckets
+  force_destroy = var.bucket_force_destroy
+  tags          = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  acl    = var.acl
 }
