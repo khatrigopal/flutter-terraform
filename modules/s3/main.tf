0c410c9099773efa53c50b65ac7c686b8a84b030
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-28 22:20:30 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,4 @@
-resource "aws_s3_bucket" "s3-bucket" {
-  bucket        = var.s3_buckets
-  force_destroy = var.bucket_force_destroy
-  tags          = var.tags
+resource "aws_s3_bucket" "demos3" {
+    bucket = "${var.bucket_name}" 
+    acl = "${var.acl_value}"   
 }
